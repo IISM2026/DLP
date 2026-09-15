61f51a0dccf7e04e9c8f3d84392795a34b3ef2e3
IISM2026 <a.muazroom@student.iium.edu.my>
2026-09-15 19:22:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const GIDS = {
   Topics: "960685870"
 };
 
-const APPS_SCRIPT_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyS7fZgd3fHbCjMw7i5iq_VU16VFt7rfK8_Uvt0Q_GbefaeqZin9gOFsRySrfqrLQpsZg/exec";
 
 const gvizUrl = (gid) =>
   `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:json&gid=${gid}`;
